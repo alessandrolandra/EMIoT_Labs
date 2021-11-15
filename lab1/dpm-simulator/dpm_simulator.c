3da63b66566a77309c6794d252a2a5d1e0f8f434
ALESSANDRO LANDRA S284939 <s284939@studenti.polito.it>
2021-11-16 00:06:28 +0100

    feat: history policy managed

--- lab1/dpm-simulator/dpm_simulator.c
+++ lab1/dpm-simulator/dpm_simulator.c
@@ -9,6 +9,14 @@
 
 #define MAX_FILENAME 256
 
+#define wl_id 2
+#define is_idle_allowed 0
+#define start_timeout 0
+#define end_timeout 200
+#define start_threshold 0
+#define end_threshold 25
+//#define TIMEOUT_POLICY//comment to use history policy
+
 int main(int argc, char *argv[]) {
 
     char fwl[MAX_FILENAME];
@@ -17,12 +25,14 @@ int main(int argc, char *argv[]) {
     dpm_history_params hparams;
     dpm_policy_t sel_policy;
     psm_time_t t_be;
-    int8_t wl_id = 2;
-    int8_t is_idle_allowed = 0;
 
-    if(argc==1){
+    if(argc==1) {
         init_params(fwl, &psm, &t_be, wl_id, is_idle_allowed);
-        simulate_different_timeouts(fwl, psm, &sel_policy, &tparams, &hparams, is_idle_allowed, t_be);
+        #ifdef TIMEOUT_POLICY
+            simulate_different_timeouts(fwl, psm, &sel_policy, &tparams, &hparams, is_idle_allowed, t_be, start_timeout, end_timeout);
+        #else
+            simulate_different_history_thresholds(fwl, psm, &sel_policy, &tparams, &hparams, is_idle_allowed, t_be, start_threshold, end_threshold);
+        #endif
     }else{
         if(!parse_args(argc, argv, fwl, &psm, &sel_policy, &tparams, &hparams)) {
             printf("[error] reading command line arguments\n");
