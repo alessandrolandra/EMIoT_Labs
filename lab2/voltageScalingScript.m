822c394ee41f08854c1c3490ad5bcf83ec2e5d3b
Alessandro Landra <landra.alessandro@gmail.com>
2021-12-19 21:51:23 +0100

    refactor: brightness parameter defined

--- lab2/voltageScalingScript.m
+++ lab2/voltageScalingScript.m
@@ -5,9 +5,11 @@ clc;
 addpath([pwd, filesep, 'functions'])
 
 SATURATED = 1;
-Vdd1 = 12;
-Vdd2 = 12;
-b = 0.1;
+Vdd0 = 15; % original Vdd is 15
+Vdd = 12;
+
+%% Empirically determined brightness compensation parameter
+b = (Vdd0-Vdd)/12;
 
 A = imread("images/4.2.03.tiff");
 
@@ -19,19 +21,19 @@ C = hsv2rgb255(B2);
 % load from the examples
 % load('currentSamples/sample_cell_current.mat');
 
-ImatrixOriginal = currentMat(A,Vdd1);
-ImatrixModified = currentMat(C,Vdd1);
-displayedRGBOriginal = displayed_image(ImatrixOriginal, Vdd2, SATURATED);
-displayedRGBModified = displayed_image(ImatrixModified, Vdd2, SATURATED);
+ImatrixOriginal = currentMat(A,Vdd);
+ImatrixModified = currentMat(C,Vdd);
+displayedRGBOriginal = displayed_image(ImatrixOriginal, Vdd, SATURATED);
+displayedRGBModified = displayed_image(ImatrixModified, Vdd, SATURATED);
 
 % During the lesson he said not to use distortion...bah...
 %image_RGB_distorted = displayed_image(I_cell_sample, Vdd, 2);
 
-subplot(3,1,1)
+subplot(1,3,1)
 imshow(A);
-subplot(3,1,2)
+subplot(1,3,2)
 image(displayedRGBOriginal/255);
-subplot(3,1,3)
+subplot(1,3,3)
 image(displayedRGBModified/255);
 fprintf("Image distortion: %f%%\n",matDistP(B1,B2));
-fprintf("Power saving: %f%%\n\n",vsSavingP(ImatrixOriginal,ImatrixModified,Vdd2));
+fprintf("Power saving: %f%%\n\n",vsSavingP(ImatrixOriginal,ImatrixModified,Vdd)); % negative due to the fact that we are increasing the quality..
