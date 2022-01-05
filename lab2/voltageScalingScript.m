9d401afd562fad950cf0291758e8736b172177fa
Alessandro Landra <landra.alessandro@gmail.com>
2022-01-05 15:21:38 +0100

    refactor: distortion computed after display

--- lab2/voltageScalingScript.m
+++ lab2/voltageScalingScript.m
@@ -6,7 +6,7 @@ addpath([pwd, filesep, 'functions'])
 
 SATURATED = 1;
 Vdd0 = 15; % original Vdd is 15
-Vdd = 12;
+Vdd = 11;
 
 %% Empirically determined brightness compensation parameter (-> SSIM > 0.9) from Vdd = 14 downto 8
 bArr = [0.175,0.175,0.2,0.2,0.2,0.225,0.25];
@@ -29,18 +29,18 @@ for k = 1:length(imgs)
         B2 = B1;
         B2(:,:,3) = B1(:,:,3)+b;%Brightness compensation
         C = hsv2rgb255(B2);
-        L2 = rgb2lab(C);
         
         D = dvsThresholding(A,Vdd);
-        B3 = rgb2hsv(D);
-        L3 = rgb2lab(D);
         
         ImatrixOriginal = currentMat(A,Vdd);
         ImatrixModified = currentMat(C,Vdd);
         ImatrixModifiedTh = currentMat(D,Vdd);
         displayedRGBOriginal = displayed_image(ImatrixOriginal, Vdd, SATURATED);
+        LdisplayedOri = rgb2lab(displayedRGBOriginal/255);
         displayedRGBModified = displayed_image(ImatrixModified, Vdd, SATURATED);
+        LdisplayedM = rgb2lab(displayedRGBModified/255);
         displayedRGBModifiedTh = displayed_image(ImatrixModifiedTh, Vdd, SATURATED);
+        LdisplayedMTh = rgb2lab(displayedRGBModifiedTh/255);
         
         fprintf("Figure %d\n\n",n);
         figure
@@ -49,12 +49,14 @@ for k = 1:length(imgs)
         title("Original");
         subplot(2,2,2)        
         image(displayedRGBOriginal/255);
+        dvsDist = matDistP(L1,LdisplayedOri);
         title("DVS only");
-        fprintf("No brightness compensation\nPower saving: %f%%\n\n",vsSavingP(ImatrixOriginal,Vdd0,ImatrixOriginal,Vdd));
+        fprintf("No brightness compensation\nImage distortion: %f%%\n",dvsDist);
+        fprintf("Power saving: %f%%\n\n",vsSavingP(ImatrixOriginal,Vdd0,ImatrixOriginal,Vdd));
         subplot(2,2,3)
         image(displayedRGBModified/255);
         title("Fixed brightness comp");
-        fixedDist = matDistP(L1,L2);
+        fixedDist = matDistP(L1,LdisplayedM);
         fixedDistSum = fixedDistSum + fixedDist;
         fixedPS = vsSavingP(ImatrixOriginal,Vdd0,ImatrixModified,Vdd);
         fixedPSSum = fixedPSSum + fixedPS;
@@ -63,7 +65,7 @@ for k = 1:length(imgs)
         subplot(2,2,4)        
         image(displayedRGBModifiedTh/255);
         title("Thresholded brightness comp");
-        thresholdedDist = matDistP(L1,L3);
+        thresholdedDist = matDistP(L1,LdisplayedMTh);
         thresholdedDistSum = thresholdedDistSum + thresholdedDist;
         thresholdedPS = vsSavingP(ImatrixOriginal,Vdd0,ImatrixModifiedTh,Vdd);
         thresholdedPSSum = thresholdedPSSum + thresholdedPS;
