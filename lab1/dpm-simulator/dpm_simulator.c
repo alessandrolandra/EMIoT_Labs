24bc54d6136140c11525484aebb034f97eafefe0
Alessandro Landra <landra.alessandro@gmail.com>
2022-01-08 22:30:15 +0100

    refactor: commented part uncommented

--- lab1/dpm-simulator/dpm_simulator.c
+++ lab1/dpm-simulator/dpm_simulator.c
@@ -5,19 +5,19 @@
 #include "inc/psm.h"
 #include "inc/dpm_policies.h"
 #include "inc/utilities.h"
-//#include "inc/dpm_sim_auto.h"
+#include "inc/dpm_sim_auto.h"
 
 #define MAX_FILENAME 256
 #define is_idle_allowed 1
 
-/*
+
 #define wl_id 3
 #define start_timeout 0
 #define end_timeout 10
 #define start_threshold 10
 #define end_threshold 15
 #define TIMEOUT_POLICY//comment to use history policy
-*/
+
 
 int main(int argc, char *argv[]) {
 
@@ -28,7 +28,6 @@ int main(int argc, char *argv[]) {
     dpm_policy_t sel_policy;
     psm_time_t t_be;
 
-	/*
 
     if(argc==1) {
         init_params(fwl, &psm, &t_be, wl_id, is_idle_allowed);
@@ -47,15 +46,5 @@ int main(int argc, char *argv[]) {
         psm_print(psm);
         dpm_simulate(psm, sel_policy, tparams, hparams, fwl, is_idle_allowed);
     }
-	*/
-    if(!parse_args(argc, argv, fwl, &psm, &sel_policy, &tparams, &hparams)) {
-            printf("[error] reading command line arguments\n");
-            return -1;
-        }
-        init_tbe(psm,&t_be,is_idle_allowed);
-
-        psm_print(psm);
-        dpm_simulate(psm, sel_policy, tparams, hparams, fwl, is_idle_allowed);
-
     return 0;
 }
